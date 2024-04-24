fb89e7dfa3adfaed4ee685e0845ff74bb4fea126
Gravestam <github@gravestam.site>
2024-04-24 20:54:53 +0200

    U

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -16,24 +16,24 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "tokyonight_night"
 config.window_padding = {
-	left = "1cell",
-	right = "1cell",
-	top = "0.5cell",
-	bottom = "0.5cell",
+	left = "10px",
+	right = "10px",
+	top = "10px",
+	bottom = "10px",
 }
 
 config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
+-- config.window_decorations = "RESIZE"
 config.enable_scroll_bar = false
 
 config.enable_wayland = false
 
 config.front_end = "WebGpu"
-config.webgpu_power_preference = "HighPerformance"
-config.webgpu_preferred_adapter = getDiscreteGpu()
-config.max_fps = 60
+-- config.webgpu_power_preference = "HighPerformance"
+-- config.webgpu_preferred_adapter = getDiscreteGpu()
+-- config.max_fps = 60
 
-config.window_background_opacity = 1
+-- config.window_background_opacity = 1
 
 config.font = wezterm.font("JetBrains Mono Nerd Font Mono")
 config.font_size = 11.0
